c18d50985e7e082f1ba3ab621a96229d68a5619b
Emil Erasmus <lime.erasmus@gmail.com>
2023-11-29 09:36:55 +0200

    Included genre mapping.

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,6 +4,28 @@ import { Link } from "react-router-dom";
 import formatDate from "../functions/formatDate";
 
 export default function Preview(props) {
+  const genres = [
+    "Personal Growth",
+    "True Crime and Investigative Journalism",
+    "History",
+    "Comedy",
+    "Entertainment",
+    "Business",
+    "Fiction",
+    "News",
+    "Kids and Family",
+  ];
+  const vals = (props.genres).map((item) => genres[item])
+  // console.log((props.genres).forEach((currentValue, index) => genres[index]))
+  
+  // props.genres.forEach(index => {
+  //   // Check if the index is within the bounds of the genres array
+  //   if (index >= 0 && index < genres.length) {
+  //     return genres[index];
+  //   } else {
+  //     console.log(`Index ${index} is out of bounds for the genres array.`);
+  //   }
+  // });
   return (
     <Link to={`/shows/${props.id}`}>
       <div className="list__button">
@@ -18,6 +40,7 @@ export default function Preview(props) {
             <div className="preview__info">
               <h3 className="preview__title">{props.title}</h3>
               <div className="preview__author">Seasons: {props.seasons}</div>
+              <div className="preview__genres">Genres: {vals.join(", ")}</div>
               <p className="preview_hidden" id="description">
                 {props.description}
               </p>
