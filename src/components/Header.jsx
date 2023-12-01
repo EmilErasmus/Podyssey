b777eceef92a2d8c8fb5755b5b73f4950fe199ea
Emil Erasmus <lime.erasmus@gmail.com>
2023-12-01 12:12:05 +0200

    Images path update.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,14 @@ import "../styles/Header.css";
 import SearchBar from "./SearchBar.jsx";
 
 export default function Header(props) {
+
+
+  const imageUrl = 'https://podyssey.netlify.app/spartan.png'
   return (
     <header>
       <div className="hero">
         <Link className="hero__link" to="/shows">
-          <img src="../public/spartan.png" className="hero__photo" />
+          <img src={imageUrl} className="hero__photo" />
         </Link>
         <h1 className="hero__header">Podyssey</h1>
       </div>
@@ -20,3 +23,7 @@ export default function Header(props) {
     </header>
   );
 }
+
+// const imageUrl = `https://your-netlify-site-url/.netlify/large-media/images/example.jpg?nf_resize=fit&w=400`;
+
+// return <img src={imageUrl} alt="Example" />;
