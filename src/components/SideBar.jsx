a99674cf8f57ae9be2d2c9ac1e4b34783212ddf1
Emil Erasmus <lime.erasmus@gmail.com>
2023-12-01 11:21:50 +0200

    Added sidebar for navigation.

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+import '../styles/Sidebar.css';
+
+export default function Sidebar({ isOpen, onClose }) {
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <button className="close-btn" onClick={onClose}>
+        &times;
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/shows" onClick={onClose}>Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/favourites" onClick={onClose}>Favourites</Link>
+          </li>
+          {/* Add more links as needed */}
+        </ul>
+      </nav>
+    </div>
+  );
+}
