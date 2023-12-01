2fdfb675461c215b3bca8ab0f6e8127048d735c9
Emil Erasmus <lime.erasmus@gmail.com>
2023-12-01 11:18:12 +0200

    Added supabase config.

---
+++ src/supabase/supabaseClient.js
@@ -0,0 +1,10 @@
+// import { createClient } from '@supabase/supabase-js'
+
+// // Create a single supabase client for interacting with your database
+// export const supabase = createClient(
+
+//   process.env.REACT_APP_SUPABASE_URL,
+
+//   process.env.REACT_APP_SUPABASE_ANON_KEY
+
+// )
